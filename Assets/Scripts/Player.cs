5ce718487ec2f624b068caa47867ec337ed7dc61
josh <48034855+JDJoffe@users.noreply.github.com>
2019-09-14 17:22:51 +1000

    character move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 5f;
     public float jumpHeight = 5f;
-    public float groundDistance;
+    public float groundDistance =10f;
     public bool isGrounded;
     public Rigidbody rigid;
   
@@ -19,25 +19,49 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         
-      
-       
-    }
-    public void FixedUpdate()
-    {
-      
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+
+        if (groundDistance <=1 )
         {
-            rigid.velocity = new Vector3(0,jumpHeight,0);
+            isGrounded = true;
+        }
+        else { isGrounded = false; }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigid.velocity = new Vector3(0, jumpHeight, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.velocity = transform.forward * moveSpeed;
+           
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.velocity = -transform.right * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.velocity = -transform.forward * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigid.velocity = transform.right * moveSpeed;
         }
     }
-    public void OnDrawGizmos()
+    private void OnDrawGizmos()
     {
-        Physics.Raycast(transform.position, -Vector3.up, groundDistance + 0.1f);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            groundDistance = hit.distance;
+            Debug.DrawLine(transform.position, hit.point, Color.cyan);
+        }
     }
-    public void IsGrounded()
-    {
-        isGrounded = Physics.Raycast(transform.position, -Vector3.up, groundDistance + 0.1f);
-    }
+
+
+    
+   
 
 }
